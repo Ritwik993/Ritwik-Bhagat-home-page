61a2dafd07633f8687830435e11cd34d9f311af0
Ritwik993 <ritwik2025@gmail.com>
2024-11-07 10:51:20 +0530

    files added

--- src/components/Reason2.tsx
+++ src/components/Reason2.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 const Reason2=()=>{
     return(
         <div className="flex md:flex-row flex-col-reverse justify-between items-center gap-6">
-            <div className="right md:ml-[0px] ml-[60px]">
+            <div className="right md:w-auto w-[100vw] md:px-[0px] px-[10px]">
                 <Image src="/sync 1.svg" alt="" width={50} height={50}/>
                 <h2 className="text-[#2F327D] font-semibold text-[26px] mb-[20px] text-left">Fresh data updates everyday</h2>
-                <p className="w-[490px] text-[16px] text-[#595B97] text-left">BrandNav updates its data every day, providing the most recent and accurate information to its users. With a constant stream of new information, BrandNav ensures that users have access to the latest contact, firmographics, and technographics data for better decision-making. </p>
+                <p className="md:w-[490px] text-[16px] text-[#595B97] text-left">BrandNav updates its data every day, providing the most recent and accurate information to its users. With a constant stream of new information, BrandNav ensures that users have access to the latest contact, firmographics, and technographics data for better decision-making. </p>
             </div>
             <Image src="/image2.svg" alt="" width={437} height={407}/>
         </div>
--- src/components/Reason3.tsx
+++ src/components/Reason3.tsx
@@ -4,10 +4,10 @@ const Reason3=()=>{
     return(
         <div className="flex md:flex-row flex-col justify-between items-center gap-6">
             <Image src="/image3.svg" alt="" width={437} height={407}/>
-            <div className="right md:ml-[0px] ml-[60px]">
+            <div className="right md:w-auto w-[100vw] md:px-[0px] px-[10px]">
                 <Image src="/filter (2) 1.svg" alt="" width={50} height={50}/>
                 <h2 className="text-[#2F327D] font-semibold text-[26px] mb-[20px] text-left">Advanced search with 25+ unique filters</h2>
-                <p className="w-[490px] text-[16px] text-[#595B97] text-left">BrandNav offers over 25+ filters to help users build highly segmented lists for better prospecting. With BrandNav, you can create highly targeted lists that are tailored to your specific needs, streamlining your workflow and maximizing your productivity.</p>
+                <p className="md:w-[490px] text-[16px] text-[#595B97] text-left">BrandNav offers over 25+ filters to help users build highly segmented lists for better prospecting. With BrandNav, you can create highly targeted lists that are tailored to your specific needs, streamlining your workflow and maximizing your productivity.</p>
             </div>
         </div>
     )
